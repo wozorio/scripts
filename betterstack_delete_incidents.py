b760a5fad57a87a87021e8b35a80cb5f9f5215d8
Wellington Ozorio <wozorio@duck.com>
2026-07-01 13:47:01 -0300

    Delete only not resolved incidents

--- betterstack_delete_incidents.py
+++ betterstack_delete_incidents.py
@@ -44,7 +44,7 @@ def log(message: str) -> None:
 def get_incidents(api: UptimeAPI, name: str) -> list[Incident]:
     """Return all incidents whose name matches the provided name argument."""
     incidents = Incident.get_all_instances(api)
-    return [incident for incident in incidents if name == incident.name]
+    return [incident for incident in incidents if name == incident.name and incident.status.lower() != "resolved"]
 
 
 def delete_incident(incident: Incident) -> None:
